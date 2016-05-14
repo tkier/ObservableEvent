bd70e7c1a736aa3d9049ef1e4e53b262c78bf6b9
Tom Kier <tom@endlesswavesoftware.com>
2016-05-14 16:17:28 -0700

    Add xcode project.

---
+++ ObservableEvent/ObservableEvent.h
@@ -0,0 +1,19 @@
+//
+//  ObservableEvent.h
+//  ObservableEvent
+//
+//  Created by Tom Kier on 5/14/16.
+//  Copyright © 2016 Endless Wave Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObservableEvent.
+FOUNDATION_EXPORT double ObservableEventVersionNumber;
+
+//! Project version string for ObservableEvent.
+FOUNDATION_EXPORT const unsigned char ObservableEventVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObservableEvent/PublicHeader.h>
+
+
